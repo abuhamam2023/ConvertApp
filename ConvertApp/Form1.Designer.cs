4de515e022ae73206f097c5948f868eb437fe83e
AbuHamam <AbuHamam@DESKTOP-7PTOFTP>
2024-04-13 10:53:03 +0300

    Add project files.

---
+++ ConvertApp/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ConvertApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnload = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnload
+            // 
+            this.btnload.Location = new System.Drawing.Point(245, 310);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(109, 54);
+            this.btnload.TabIndex = 0;
+            this.btnload.Text = "Uplaod Image";
+            this.btnload.UseVisualStyleBackColor = true;
+            this.btnload.Click += new System.EventHandler(this.btnload_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 54);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Save as Ico";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Image = global::ConvertApp.Properties.Resources.file_upload_icon_12;
+            this.pictureBox.Location = new System.Drawing.Point(48, 23);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(306, 266);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 406);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnload);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert To Ico";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ ConvertApp/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All Images |*.png ;*.jpg;*.jpeg";
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox.Image=new Bitmap(ofd.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Icon File(*.ico)|*.ico";
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                Icon.FromHandle(((Bitmap)pictureBox.Image).GetHicon()).Save(new FileStream(sfd.FileName, FileMode.Create));
+                MessageBox.Show("Success Convert");
+                pictureBox.Image = null;
+            }
+        }
+    }
+}
